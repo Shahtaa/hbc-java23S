209abc44290973f333be57ab5d00870768084e54
Alexei <>
2023-11-19 18:30:18 +0200

    tehtavalista ok

---
+++ osa06-Osa06_10.Tehtavalista/src/main/java/Tehtavalista.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Tehtavalista {
+    private ArrayList<String> tehtavat;
+
+    public Tehtavalista() {
+        this.tehtavat = new ArrayList<>();
+    }
+
+    public void lisaa(String tehtava) {
+        this.tehtavat.add(tehtava);
+    }
+
+    public void tulosta() {
+        for (int i = 0; i < tehtavat.size(); i++) {
+            System.out.println(i + 1 + ": " + tehtavat.get(i));
+
+        }
+    }
+
+    public void poista(int numero) {
+
+        this.tehtavat.remove(tehtavat.get(numero));
+    }
+}
