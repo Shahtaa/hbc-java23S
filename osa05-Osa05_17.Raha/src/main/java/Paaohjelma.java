dbc10d61bf74f9022f53cc88deba17921bcd6fc7
Alexei <>
2023-11-23 14:46:17 +0200

    Raha

--- osa05-Osa05_17.Raha/src/main/java/Paaohjelma.java
+++ osa05-Osa05_17.Raha/src/main/java/Paaohjelma.java
@@ -2,6 +2,6 @@
 public class Paaohjelma {
 
     public static void main(String[] args) {
-        // tee tänne koodia jolla testaat ohjelmoimasi luokan toimintaa
+
     }
 }
--- osa05-Osa05_17.Raha/src/main/java/Raha.java
+++ osa05-Osa05_17.Raha/src/main/java/Raha.java
@@ -7,8 +7,8 @@ public class Raha {
     public Raha(int euroa, int senttia) {
 
         if (senttia > 99) {
-            euroa = euroa + senttia / 100;
-            senttia = senttia % 100;
+            euroa += senttia / 100;
+            senttia %= 100;
         }
 
         this.euroa = euroa;
@@ -16,20 +16,58 @@ public class Raha {
     }
 
     public int eurot() {
-        return this.euroa;
+        return euroa;
     }
 
     public int sentit() {
-        return this.senttia;
+        return senttia;
     }
 
     public String toString() {
         String nolla = "";
-        if (this.senttia < 10) {
+        if (senttia < 10) {
             nolla = "0";
         }
 
-        return this.euroa + "." + nolla + this.senttia + "e";
+        return euroa + "." + nolla + senttia + "e";
     }
 
+    public Raha plus(Raha lisattava) {
+        int eurojenSumma = this.eurot() + lisattava.eurot();
+        int senttienSumma = this.sentit() + lisattava.sentit();
+
+        while (senttienSumma > 100) {
+            eurojenSumma++;
+            senttienSumma -= 100;
+        }
+        Raha uusi = new Raha(eurojenSumma, senttienSumma);
+
+        return uusi;
+    }
+
+    public boolean vahemman(Raha verrattava) {
+        if (this.eurot() < verrattava.eurot()) {
+            return true;
+        } else if (this.eurot() == verrattava.eurot() && this.sentit() < verrattava.sentit()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Raha miinus(Raha vahentaja) {
+        int eurojenSumma = this.eurot() - vahentaja.eurot();
+        int senttienSumma = this.sentit() - vahentaja.sentit();
+
+        if (eurojenSumma < 0) {
+            eurojenSumma = 0;
+            senttienSumma = 0;
+        } else if (eurojenSumma > 0 && senttienSumma < 0) {
+            eurojenSumma--;
+            senttienSumma += 100;
+        }
+        Raha uusi = new Raha(eurojenSumma, senttienSumma);
+
+        return uusi;
+    }
 }
