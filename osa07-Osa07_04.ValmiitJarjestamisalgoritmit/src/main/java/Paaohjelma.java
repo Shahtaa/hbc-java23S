f30992a2addbd3186ee14e5259b8f620b3bc3871
Alexei <>
2023-11-29 14:27:59 +0200

    07_04

--- osa07-Osa07_04.ValmiitJarjestamisalgoritmit/src/main/java/Paaohjelma.java
+++ osa07-Osa07_04.ValmiitJarjestamisalgoritmit/src/main/java/Paaohjelma.java
@@ -1,10 +1,34 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Paaohjelma {
 
     public static void main(String[] args) {
         // tee testikoodia tänne
-        int[] taulukko = {3, 1, 5, 99, 3, 12};
+        ArrayList<Integer> luvut = new ArrayList<>();
+        luvut.add(3);
+        luvut.add(2);
+        luvut.add(0);
+        luvut.add(1);
+        jarjestaLuvut(luvut);
+        System.out.println(luvut);
+    }
+
+    public static void jarjesta(int[] taulukko) {
+        Arrays.sort(taulukko);
+    }
+
+    public static void jarjesta(String[] taulukko) {
+        Arrays.sort(taulukko);
+    }
+
+    public static void jarjestaLuvut(ArrayList<Integer> luvut) {
+        Collections.sort(luvut);
+    }
+
+    public static void jarjestaMerkkijonot(ArrayList<String> merkkijonot) {
+        Collections.sort(merkkijonot);
     }
 
 }
