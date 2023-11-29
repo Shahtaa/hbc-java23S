612de5d0c4888e284aeef236f9b9d168ecb27cf1
Alexei <>
2023-11-29 12:21:37 +0200

    07_02

--- osa07-Osa07_02.NestesailiotOlioilla/src/main/java/NestesailiotOlioilla.java
+++ osa07-Osa07_02.NestesailiotOlioilla/src/main/java/NestesailiotOlioilla.java
@@ -1,16 +1,16 @@
-
 import java.util.Scanner;
 
 public class NestesailiotOlioilla {
 
     public static void main(String[] args) {
-
         Scanner lukija = new Scanner(System.in);
+
         Sailio ensimmainen = new Sailio();
         Sailio toinen = new Sailio();
+
         while (true) {
-            System.out.println("Ensimmäinen: " + ensimmainen.sisalto() + "/100");
-            System.out.println("Toinen: " + toinen.sisalto() + "/100");
+            System.out.println("Ensimmäinen: " + ensimmainen);
+            System.out.println("Toinen: " + toinen);
             System.out.print("> ");
 
             String luettu = lukija.nextLine();
@@ -22,29 +22,24 @@ public class NestesailiotOlioilla {
             luettu = osat[0];
             int maara = Integer.valueOf(osat[1]);
 
-            if (luettu.equals("lisaa") && maara > 0) {
+            if (luettu.equals("lisaa")) {
                 ensimmainen.lisaa(maara);
             }
 
-            if (luettu.equals("siirra") && maara > 0) {
-                if (maara <= ensimmainen.sisalto()) {
-
-                    ensimmainen.poista(maara);
-
-                    toinen.lisaa(maara);
-                } else {
-                    toinen.lisaa(ensimmainen.sisalto());
-                    ensimmainen.poista(maara);
-                    ;
+            if (luettu.equals("siirra")) {
+                if (maara > ensimmainen.sisalto()) {
+                    maara = ensimmainen.sisalto();
                 }
 
+                ensimmainen.poista(maara);
+                toinen.lisaa(maara);
             }
 
-            if (luettu.equals("poista") && maara > 0) {
+            if (luettu.equals("poista")) {
                 toinen.poista(maara);
-
             }
 
+            System.out.println("");
         }
     }
 
