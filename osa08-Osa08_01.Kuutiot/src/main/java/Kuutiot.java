818ab06a25f7ea17c3837c3165d140706f7824c2
Alexei <>
2023-11-30 10:57:20 +0200

    08_01

--- osa08-Osa08_01.Kuutiot/src/main/java/Kuutiot.java
+++ osa08-Osa08_01.Kuutiot/src/main/java/Kuutiot.java
@@ -6,5 +6,16 @@ public class Kuutiot {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
+        while (true) {
+            String syote = lukija.nextLine();
+            if (syote.equals("loppu")) {
+                break;
+            }
+
+            int i = Integer.parseInt(syote);
+            System.out.println(i * i * i);
+
+        }
+
     }
 }
