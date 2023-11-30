2775beb8336da7c9d38f3f42835aa09c65493fa4
Alexei <>
2023-11-30 10:27:20 +0200

    07_07 kesken

--- osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
+++ osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,7 +8,28 @@ public class Reseptihaku {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        System.out.print("Mitä luetaan? ");
+        String syote = lukija.nextLine();
+
+        try (Scanner tiedostonLukija = new Scanner(Paths.get(syote))) {
+            while (true) {
+                System.out.println("");
+                System.out.print("Komennot:\nlistaa - listaa reseptit\nlopeta - lopettaa ohjelman\nSyötä komento: ");
+                syote = lukija.nextLine();
+                if (syote.equals("listaa")) {
+                    while (tiedostonLukija.hasNextLine()) {
+                        System.out.println(tiedostonLukija.nextLine());
+                    }
+                }
+                if (syote.equals("lopeta")) {
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Tiedoston lukeminen epäonnstui.");
+
+        }
 
     }
-
 }
