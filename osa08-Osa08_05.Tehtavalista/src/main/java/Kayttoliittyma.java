5bda8b5211570af807118f5ee3462f73a2b8803f
Alexei <>
2023-11-30 12:24:22 +0200

    08_05

--- osa08-Osa08_05.Tehtavalista/src/main/java/Kayttoliittyma.java
+++ osa08-Osa08_05.Tehtavalista/src/main/java/Kayttoliittyma.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Kayttoliittyma {
+import java.util.Scanner;
+
+public class Kayttoliittyma {
+    private Scanner lukija;
+    private Tehtavalista lista;
+
+    public Kayttoliittyma(Tehtavalista lista, Scanner lukija) {
+        this.lista = lista;
+        this.lukija = lukija;
+    }
+
+    public void kaynnista() {
+        while (true) {
+            System.out.print("Komento:");
+            String syote = this.lukija.nextLine();
+            if (syote.equals("lisaa")) {
+                System.out.println("Lisättävä:");
+                String sana = this.lukija.nextLine();
+                this.lista.lisaa(sana);
+            }
+            if (syote.equals("listaa")) {
+                this.lista.tulosta();
+            }
+
+            if (syote.equals("poista")) {
+                System.out.println("Mikä poistetaan?");
+                int numero = this.lukija.nextInt();
+                this.lista.poista(numero);
+
+            }
+            if (syote.equals("lopeta")) {
+                break;
+            }
+        }
+    }
 }
