306997c344275135c77d51eea5da9dc46bb11670
Alexei <>
2023-11-30 12:43:49 +0200

    08_06

--- osa08-Osa08_06.Lempinimet/src/main/java/Lempinimet.java
+++ osa08-Osa08_06.Lempinimet/src/main/java/Lempinimet.java
@@ -5,6 +5,11 @@ public class Lempinimet {
 
     public static void main(String[] args) {
         // Tee tehtävänannossa pyydetyt operaatiot täällä!
+        HashMap<String, String> lempinimet = new HashMap<>();
+        lempinimet.put("matti", "mage");
+        lempinimet.put("arto", "arppa");
+        lempinimet.put("mikael", "mixu");
+        System.out.println(lempinimet.get("mikael"));
     }
 
 }
