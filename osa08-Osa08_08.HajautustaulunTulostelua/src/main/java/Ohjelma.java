b1acd09be0744cb6d9badd50e2e09e8a00ecbc6d
Alexei <>
2023-12-01 10:48:43 +0200

    08_08

--- osa08-Osa08_08.HajautustaulunTulostelua/src/main/java/Ohjelma.java
+++ osa08-Osa08_08.HajautustaulunTulostelua/src/main/java/Ohjelma.java
@@ -1,10 +1,41 @@
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Ohjelma {
 
     public static void main(String[] args) {
         // Testaa ohjelmasi toimintaa täällä!
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put("a", "b");
+        hm.put("c", "d");
+        hm.put("e", "f");
+        tulostaAvaimetJoissa(hm, "a");
+
+    }
+
+    public static void tulostaAvaimet(HashMap<String, String> taulu) {
+        for (String avain : taulu.keySet()) {
+            System.out.println(avain);
+        }
+    }
+
+    public static void tulostaAvaimetJoissa(HashMap<String, String> taulu, String string) {
+
+        for (String haunkohde : taulu.keySet()) {
+            if (haunkohde.contains(string)) {
+                System.out.println(haunkohde);
+            }
+        }
+
+    }
+
+    public static void tulostaArvotJosAvaimessa(HashMap<String, String> taulu, String string) {
+        for (String haunkohde : taulu.keySet()) {
+            if (haunkohde.contains(string)) {
+                System.out.println(taulu.get(haunkohde));
+            }
+        }
     }
 
 }
