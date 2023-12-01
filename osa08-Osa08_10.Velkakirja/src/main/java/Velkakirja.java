8f883658cd01ec4ca5c56b0b437a78475ca392fb
Alexei <>
2023-12-01 13:43:01 +0200

    08_10

---
+++ osa08-Osa08_10.Velkakirja/src/main/java/Velkakirja.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Velkakirja {
+    private HashMap<String, Double> velat;
+
+    public Velkakirja() {
+        this.velat = new HashMap<>();
+    }
+
+    public void asetaLaina(String kenelle, double maara) {
+
+    }
+
+    public double paljonkoVelkaa(String kuka) {
+        return 1;
+    }
+
+}
